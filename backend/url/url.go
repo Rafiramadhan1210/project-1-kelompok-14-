bf830a75986bbedde6d12d60901c9225d0c351d3
Rafiramadhan1210 <rrafi2008@gmail.com>
2026-07-18 00:05:57 +0700

    memperbaiki url.go

--- backend/url/url.go
+++ backend/url/url.go
@@ -7,7 +7,6 @@ import (
 )
 
 func Web(page *fiber.App) {
-	page.Get("/", controller.Homepage)
 	page.Get("/ip", controller.GetIPServer)
 	page.Get("/whatsauth/refreshtoken", controller.RefreshWAToken)
 	page.Post("/whatsauth/webhook", controller.WhatsAuthReceiver)
